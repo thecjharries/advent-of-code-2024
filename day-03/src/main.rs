2d608f64eb94473a575ce6a9daa523f1c7482519
CJ Harries <thecjharries@users.noreply.github.com>
2024-12-08 14:39:49 -0600

    feat/day 03 (#3)
    * Add day 03 boilerplate
    
    * Solve part 1
    
    * Fix linting
    
    * Solve day 3

---
+++ day-03/src/main.rs
@@ -0,0 +1,77 @@
+// Copyright 2024 CJ Harries
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use regex::Regex;
+use std::fs::read_to_string;
+
+#[cfg(not(tarpaulin_include))]
+fn main() {
+    let input = read_to_string("input.txt").expect("Unable to read input file");
+    println!("Part 1: {}", part1(input.clone()));
+    println!("Part 2: {}", part2(input));
+}
+
+fn part1(input: String) -> usize {
+    let mul_pattern = Regex::new(r"mul\((?<first>\d{1,3}),(?<second>\d{1,3})\)").unwrap();
+    input
+        .lines()
+        .map(|line| {
+            mul_pattern
+                .captures_iter(line)
+                .map(|capture| {
+                    let first = capture
+                        .name("first")
+                        .unwrap()
+                        .as_str()
+                        .parse::<usize>()
+                        .unwrap();
+                    let second = capture
+                        .name("second")
+                        .unwrap()
+                        .as_str()
+                        .parse::<usize>()
+                        .unwrap();
+                    first * second
+                })
+                .sum::<usize>()
+        })
+        .sum()
+}
+
+fn part2(input: String) -> usize {
+    let remove_pattern = Regex::new(r"don't\(\).*?do\(\)|don't\(\).*$").unwrap();
+    let single_line = input.replace("\n", "");
+    let cleaned = remove_pattern.replace_all(&single_line, "");
+    part1(cleaned.to_string())
+}
+
+#[cfg(not(tarpaulin_include))]
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        let input =
+            "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))".to_string();
+        assert_eq!(161, part1(input));
+    }
+
+    #[test]
+    fn test_part2() {
+        let input =
+            "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))".to_string();
+        assert_eq!(48, part2(input));
+    }
+}
