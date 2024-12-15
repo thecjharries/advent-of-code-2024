6fdd8caf9dd82cf0c467c015ca4e456d131a48f2
CJ Harries <cj@wizardsoftheweb.pro>
2024-12-15 17:50:34 -0600

    Test part 2

--- day-05/src/main.rs
+++ day-05/src/main.rs
@@ -72,12 +72,10 @@ fn part1(input: String) -> usize {
             rules.insert(rule.number, rule);
         }
     }
-    print!("Rules: {:?}\n", rules);
     let messages = sections.next().unwrap().split("\n");
     let mut count = 0;
     for message in messages {
         let sequence: Vec<usize> = message.split(",").map(|x| x.parse().unwrap()).collect();
-        print!("Checking sequence: {:?}\n", sequence);
         let mut valid = true;
         for i in 0..sequence.len() - 1 {
             let current = sequence[i];
@@ -146,6 +144,44 @@ mod tests {
         assert_eq!(
             143,
             part1(
+                "47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47"
+                    .to_string()
+            )
+        );
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(
+            123,
+            part2(
                 "47|53
 97|13
 97|61
